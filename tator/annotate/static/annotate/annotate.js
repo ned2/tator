dd36760c1b274d9307b84a8a671402b389b8f837
Ned Letcher <nletcher@gmail.com>
2016-10-09 19:46:25 +1100

    changed layout to horizontal one

--- tator/annotate/static/annotate/annotate.js
+++ tator/annotate/static/annotate/annotate.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-    /* Apply "selected" class to the currently selected option for a form-group
-     * (removing from any previously selected option)*/
+    // Apply "selected" class to the currently selected option for a form-group
+    // (removing from any previously selected option)
     function highlightSelected() {
         if (this.checked) {
             $(this).closest(".form-group").find('input[type="radio"]').parent().removeClass("selected");
@@ -9,12 +9,15 @@ $(document).ready(function(){
         }
     }
 
+    // Radio buttons need to become highlighted when selected as well as start
+    // highlighted if already selected
     $('input[type="radio"]').change(highlightSelected).each(highlightSelected);
 
 
     // Submit annotation button is disabled initially */
     $('#annotation-form button').prop('disabled', true);
 
+    
     // Enable submit form button when form has been filled out
     $('input[type="radio"]').change(function() {
         if ($('input[type="radio"]:checked').length == 3) {
